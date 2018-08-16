6b413791a76ce88ef20333a30604e51b3c806273
vagrant <vagrant@homestead>
2018-08-16 07:35:15 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,3 +4,10 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+// 生成话题的摘录--excerp字段，将作为文章页面的 description 元标签使用，有利于 SEO 搜索引擎优化
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
