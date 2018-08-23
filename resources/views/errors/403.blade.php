16879e958e513ad73caa28c65effd01791a15362
Gu0Yu <guoyu0106@163.com>
2018-08-23 07:51:26 +0000

    Users can edit their own topic

--- resources/views/errors/403.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('title', '403')
-
-@section('content')
-    <div style="text-align: center  ">
-        <h1>403 </h1>
-        <h2>{{$result}}</h2>
-    </div>
-@stop
