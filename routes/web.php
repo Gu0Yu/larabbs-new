67f4d530314c5991dcfbcce9fec1fb4ddaf9621e
Gu0Yu <guoyu0106@163.com>
2018-09-06 06:29:07 +0000

    Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@
 
 Route::get('/timetest', 'TestController@index');
 
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 //Auth::routes() 等同于下面登录注册路由
 
